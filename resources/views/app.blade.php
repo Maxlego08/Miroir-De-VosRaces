7e9d3dcc6ea539946f53b7a03e6a6c4460afc0cc
Maxlego08 <maxlego08@sfr.fr>
2025-05-03 18:57:24 +0200

    :memo: Update header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,46 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#252539">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="description" content="Ouvrez votre coeur et laissez le miroir pénétrer votre âme">
+        <meta name="theme-color" content="#1A1A2E">
+        <meta name="author" content="Maxence (Maxlego08) Simon">
+
+        <meta name="keywords" content="miroir, vos races, seven wands, vosraces">
+        <meta property="description" content="Ouvrez votre coeur et laissez le miroir pénétrer votre âme">
+        <meta name="description" content="Ouvrez votre coeur et laissez le miroir pénétrer votre âme">
+
+        <base href="{{ route('home') }}">
+        <meta property="og:title" content="{{ config('app.name', 'Laravel') }}">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ url()->current() }}">
+        <meta property="og:image" content="">
+        <meta property="og:description" content="Ouvrez votre coeur et laissez le miroir pénétrer votre âme">
+        <meta property="og:site_name" content="Miroir de VosRaces">
+
+        <meta name="msapplication-TileColor" content="#252539">
+        <meta name="msapplication-TileImage" content="{{ asset('android-chrome-192x192.png') }}">
+
+        <meta name="author" content="GROUPEZ.DEV [contact@groupez.dev]">
+        <meta name="publisher" content="GROUPEZ.DEV [contact@groupez.dev]">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:site" content="@GroupeZ_">
+        <meta name="twitter:creator" content="@GroupeZ_">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:title" content="{{ config('app.name', 'Laravel') }}">
+        <meta name="twitter:image" content="{{ asset('android-chrome-192x192.png') }}">
+        <meta property="og:title" content="{{ config('app.name', 'Laravel') }}">
+        <meta property="og:type" content="website">
+        <meta property="og:site_name" content="Miroir de VosRaces">
+        <meta property="og:description" content="Ouvrez votre coeur et laissez le miroir pénétrer votre âme">
+        <meta property="og:url" content="{{ request()->url() }}">
+        <meta property="og:image" content="{{ asset('android-chrome-192x192.png') }}">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('ChooseHouse', ['canLogin' => Route::has('login'), 'canRegister' => Route::has('register'), 'laravelVersion' => Application::VERSION, 'phpVersion' => PHP_VERSION,]);
-});
+})->name('home');
 
 /*Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
